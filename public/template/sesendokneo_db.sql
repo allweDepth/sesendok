59098a221a6954d9d1e35c016b5a5212a5bc1e33
allweDepth <dpupr.pasangkayu@gmail.com>
2024-03-03 00:09:17 +0800

    db

--- public/template/sesendokneo_db.sql
+++ public/template/sesendokneo_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Waktu pembuatan: 02 Mar 2024 pada 05.59
+-- Waktu pembuatan: 02 Mar 2024 pada 17.08
 -- Versi server: 10.4.28-MariaDB
 -- Versi PHP: 8.2.4
 
@@ -129,7 +129,7 @@ CREATE TABLE `daftar_paket_neo` (
   `kd_opd` varchar(50) NOT NULL,
   `tahun` year(4) NOT NULL,
   `uraian` text NOT NULL,
-  `id_dppa` varchar(400) NOT NULL,
+  `id_uraian` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`id_uraian`)),
   `kd_sub_keg` text NOT NULL,
   `volume` int(11) DEFAULT NULL,
   `satuan` int(11) DEFAULT NULL,
@@ -143,6 +143,7 @@ CREATE TABLE `daftar_paket_neo` (
   `renc_output` varchar(255) DEFAULT NULL,
   `output` varchar(255) DEFAULT NULL,
   `id_rekanan` int(11) NOT NULL,
+  `nama_rekanan` varchar(255) DEFAULT NULL,
   `nama_ppk` varchar(255) DEFAULT NULL,
   `nip_ppk` int(18) DEFAULT NULL,
   `nama_pptk` varchar(255) DEFAULT NULL,
@@ -176,6 +177,7 @@ CREATE TABLE `daftar_paket_neo` (
   `file_pho` varchar(255) DEFAULT NULL,
   `file_fho` varchar(255) DEFAULT NULL,
   `file` varchar(255) DEFAULT NULL,
+  `disable` tinyint(1) DEFAULT 0,
   `setujui` tinyint(1) DEFAULT 0,
   `kunci` tinyint(1) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
@@ -439,7 +441,14 @@ CREATE TABLE `pengaturan_neo` (
   `kunci_renja_p` tinyint(1) DEFAULT NULL,
   `kunci_dppa` tinyint(1) DEFAULT NULL,
   `kunci_paket` tinyint(1) DEFAULT NULL,
-  `kunci_realisasi` tinyint(1) DEFAULT NULL
+  `kunci_realisasi` tinyint(1) DEFAULT NULL,
+  `setujui_renstra` tinyint(1) DEFAULT NULL,
+  `setujui_renja` tinyint(1) DEFAULT NULL,
+  `setujui_dpa` tinyint(1) DEFAULT NULL,
+  `setujui_renja_p` tinyint(1) DEFAULT NULL,
+  `setujui_dppa` tinyint(1) DEFAULT NULL,
+  `setujui_paket` tinyint(1) DEFAULT NULL,
+  `setujui_realisasi` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
@@ -990,7 +999,7 @@ CREATE TABLE `user_sesendok_biila` (
 
 INSERT INTO `user_sesendok_biila` (`id`, `username`, `email`, `nama`, `password`, `kd_organisasi`, `nama_org`, `kd_wilayah`, `type_user`, `photo`, `tgl_daftar`, `tgl_login`, `tahun`, `kontak_person`, `font_size`, `warna_tbl`, `scrolling_table`, `disable_login`, `disable_anggaran`, `disable_kontrak`, `disable_realisasi`, `disable_chat`, `ket`) VALUES
 (1, 'alwi_mansyur', 'alwi@gmail.com', 'Alwi Mansyur', '$2y$10$phmt521EHu3PEkilYD/TJ.i1U.ZcMjAHAJt4y88r3O0tfbgs8HQl6', '1.03.0.00.0.00.01.0000', 'Alwi Mansyur', '76.01', 'user', 'images/avatar/default.jpeg', '2018-06-04 21:57:05', '2024-01-26 14:17:26', '2024', 'pasangkayu ji', 90.00, 'non', 'short', 0, 0, 0, 0, 1, 'apa yang dapat saya berikan'),
-(2, 'nabiila', 'nabiila@gmail.com', 'nabiila', '$2y$10$Zxp6h5J9v8MiUtUZpDvNKe81qhVaN9gBTVusn/ov9mVwti/du1q1G', '1.03.0.00.0.00.01.0000', 'PT. Angin Ribat Skali dan satgat mengesankan sekali', '76.01', 'admin', 'images/avatar/bbf4f78067dad81bec03965da604932e9e18f570_2.jpg', '2018-06-09 15:54:29', '2024-03-02 04:49:16', '2024', '08128888', 80.00, 'non', 'short', 0, 0, 0, 0, 1, 'Apa yang dapat saya berikan untuk Pasangkayu'),
+(2, 'nabiila', 'nabiila@gmail.com', 'nabiila', '$2y$10$Zxp6h5J9v8MiUtUZpDvNKe81qhVaN9gBTVusn/ov9mVwti/du1q1G', '1.03.0.00.0.00.01.0000', 'PT. Angin Ribat Skali dan satgat mengesankan sekali', '76.01', 'admin', 'images/avatar/bbf4f78067dad81bec03965da604932e9e18f570_2.jpg', '2018-06-09 15:54:29', '2024-03-02 23:35:25', '2024', '08128888', 80.00, 'non', 'short', 0, 0, 0, 0, 1, 'Apa yang dapat saya berikan untuk Pasangkayu'),
 (3, 'inayah', 'inayah@gmail.com', 'inayah', '$2y$10$J1RLk2kaKqYeuFs2q76vxuoPYTi3cA8dCjRISJlnwlsi3sdHoAKg.', '', 'PT. Angin Ribat Skali dan satgat mengesankan sekali', '', 'user', 'images/avatar/default.jpeg', '2018-06-22 22:04:17', '2020-03-08 02:30:41', '2024', '', 80.00, NULL, 'short', 0, 0, 0, 0, 1, 'dimana mana hatiku senang oke'),
 (4, 'Arlinda', 'arlinda@gmail.com', 'Arlinda Achmad', '$2y$10$V.f/.ElwettBd3jyJfMR5epHT0s8NVqaU/mL8ZIqIJo.HBb.6x/Qi', '', 'Prof', '', 'admin', 'images/avatar/default.jpeg', '2018-07-10 14:27:06', '2018-10-21 12:23:09', '2024', '', 80.00, NULL, 'short', 0, 0, 0, 0, 1, 'Apa yang dapat saya berikan untuk Pasangkayu.'),
 (5, 'administrator', 'alwi.mansyur@gmail.com', 'administrator', '$2y$10$cFR8KdFGXUFBZ5C5payBEOb3aPEXtvYwAKO6Gc6Zdqyjo7WRuDY8.', '', 'administrator AHSP', '', 'user', 'images/avatar/c14719a7f71e46badf2cf93ae373ae9797281782_9.png', '2023-02-09 23:41:34', '2023-02-23 00:05:26', '2024', '08128886665', 80.00, 'non', 'short', 0, 0, 0, 0, 1, 'Apa yang dapat saya berikan untuk mu');
